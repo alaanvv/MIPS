0139f0536d06e9d0445d551fffb5260580b6f9cc
alaanvv <valealan84@gmail.com>
2025-11-29 14:07:14 -0300

    renaming mux32x32 -> mux_32x32

--- 10_mux_32x32/mux_32x32_tb.vhd
+++ 10_mux_32x32/mux_32x32_tb.vhd
@@ -3,35 +3,35 @@ use IEEE.std_logic_1164.all;
 use work.tipo.all;
 use IEEE.numeric_std.all;
 
-entity mux32x32_tb is
-end mux32x32_tb;
+entity mux_32x32_tb is
+end mux_32x32_tb;
 
-architecture tb of mux32x32_tb is
-    component mux32x32 is
+architecture tb of mux_32x32_tb is
+    component mux_32x32 is
         port (
-            E     : in  tipo_vetor_de_palavras(0 to 31);
+            I     : in  tipo_vetor_de_palavras(0 to 31);
             Sel   : in  std_logic_vector(4 downto 0);
-            Saida : out tipo_palavra
+            S : out tipo_palavra
         );
     end component;
 
-    signal E     : tipo_vetor_de_palavras(0 to 31);
+    signal I     : tipo_vetor_de_palavras(0 to 31);
     signal Sel   : std_logic_vector(4 downto 0);
-    signal Saida : tipo_palavra;
+    signal S : tipo_palavra;
 
 begin
 
-    dut: mux32x32
+    dut: mux_32x32
         port map (
-            E     => E,
+            I     => I,
             Sel   => Sel,
-            Saida => Saida
+            S => S
         );
 
     stim: process
     begin
         for i in 0 to 31 loop
-            E(i) <= std_logic_vector(to_unsigned(i, 32));
+            I(i) <= std_logic_vector(to_unsigned(i, 32));
         end loop;
 
         Sel <= "00000"; wait for 10 ns;
