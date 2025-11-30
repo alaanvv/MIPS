92bf668e90f711d5e2c199ebcae1169de1853092
alaanvv <valealan84@gmail.com>
2025-11-30 00:47:50 -0300

    alu_cu kkkkkkkkkkkkkkk

---
+++ 21_alu_cu/alu_cu.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+--
+
+entity alu_cu is
+  port ( Funct:   in  std_logic_vector(0 to 5);
+         ALUOp:   in  std_logic_vector(1 downto 0);
+         AInvert: out std_logic;
+         BInvert: out std_logic;
+         Op:      out std_logic_vector(1 downto 0) );
+end alu_cu;
+
+---
+
+architecture ar of alu_cu is
+begin
+
+process(ALUOp, Funct)
+begin
+
+if    ALUOp = "00" then
+  AInvert <= '0';
+  BInvert <= '0';
+  Op      <= "10";
+elsif ALUOp = "01" then
+  AInvert <= '0';
+  BInvert <= '1';
+elsif ALUOp = "10" then
+  if    Funct = "100000" then
+    AInvert <= '0';
+    BInvert <= '0';
+    Op      <= "10";
+  elsif Funct = "100100" then
+    AInvert <= '0';
+    BInvert <= '0';
+    Op      <= "00";
+  elsif Funct = "100111" then
+    AInvert <= '1';
+    BInvert <= '1';
+    Op      <= "00";
+  elsif Funct = "100101" then
+    AInvert <= '0';
+    BInvert <= '0';
+    Op      <= "01";
+  elsif Funct = "101010" then
+    AInvert <= '0';
+    BInvert <= '1';
+    Op      <= "11";
+  elsif Funct = "100010" then
+    AInvert <= '0';
+    BInvert <= '1';
+    Op      <= "10";
+  end if;
+end if;
+
+end process;
+
+end architecture ar;
---
+++ 21_alu_cu/alu_cu_tb.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity alu_cu_tb is
+end alu_cu_tb;
+
+architecture tb of alu_cu_tb is
+
+    component alu_cu is
+        port (
+            Funct   : in  std_logic_vector(0 to 5);
+            ALUOp   : in  std_logic_vector(1 downto 0);
+            AInvert : out std_logic;
+            BInvert : out std_logic;
+            Op      : out std_logic_vector(1 downto 0)
+        );
+    end component;
+
+    signal Funct   : std_logic_vector(0 to 5);
+    signal ALUOp   : std_logic_vector(1 downto 0);
+    signal AInvert : std_logic;
+    signal BInvert : std_logic;
+    signal Op      : std_logic_vector(1 downto 0);
+
+begin
+
+    dut: alu_cu
+        port map (
+            Funct   => Funct,
+            ALUOp   => ALUOp,
+            AInvert => AInvert,
+            BInvert => BInvert,
+            Op      => Op
+        );
+
+    stim: process
+    begin
+        ALUOp <= "00"; Funct <= "000000"; wait for 20 ns;
+        ALUOp <= "01"; Funct <= "000000"; wait for 20 ns;
+        ALUOp <= "10"; Funct <= "100000"; wait for 20 ns;
+        ALUOp <= "10"; Funct <= "100010"; wait for 20 ns;
+        ALUOp <= "10"; Funct <= "100100"; wait for 20 ns;
+        ALUOp <= "10"; Funct <= "100101"; wait for 20 ns;
+        ALUOp <= "10"; Funct <= "101010"; wait for 20 ns;
+
+        wait;
+    end process;
+
+end tb;
